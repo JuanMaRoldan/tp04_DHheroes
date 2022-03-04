642ed8120b28d3696aea250d9aa2cae97a74c698
JuanMaRoldan <juanmanuel.roldan@outlook.com>
2022-03-04 20:27:38 -0300

    vistas completadas

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require(`express`);
+const path = require(`path`);
+const app = express();
+const port = 3030;
+
+app.use(express.static(path.join(__dirname, `public`)))
+
+app.get(`/`, (req,res) => {
+    return res.sendFile(path.join(__dirname,`views`, `home.html`))
+})
+app.get(`/about`, (req,res) => {
+
+    return res.sendFile(path.join(__dirname,`views`, `about.html`))
+})
+
+app.get(`*`,(req,res) => {
+    return res.sendFile(path.join(__dirname,`views`, `notFound.html`))
+})
+
+app.listen(port , () =>console.log(`Server running on port http://localhost:${port}`));
